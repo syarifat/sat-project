7aec9aab09db4d8f17d186d89c80546c12671930
syarifat <syarifahsanit@gmail.com>
2026-01-29 20:56:25 +0700

    done 5

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
+use Illuminate\Support\Facades\DB;
 
 // Halaman Public (Landing Page)
 Route::get('/', [ProjectController::class, 'home'])->name('home');
 
 // Halaman Admin (CRUD Resource)
 Route::resource('projects', ProjectController::class);
+
+Route::get('/debug-db', function () {
+    try {
+        // 1. Cek Koneksi DB
+        DB::connection()->getPdo();
+        
+        // 2. Cek List Tabel
+        $tables = DB::select('SHOW TABLES');
+        
+        return response()->json([
+            'status' => 'OK',
+            'message' => 'Koneksi Database Berhasil! 🎉',
+            'database_name' => DB::connection()->getDatabaseName(),
+            'tables_found' => count($tables),
+            'ssl_ca_path_config' => config('database.connections.mysql.options.' . PDO::MYSQL_ATTR_SSL_CA),
+            'file_exists' => file_exists(config('database.connections.mysql.options.' . PDO::MYSQL_ATTR_SSL_CA)) ? 'YES' : 'NO'
+        ]);
+    } catch (\Exception $e) {
+        return response()->json([
+            'status' => 'ERROR',
+            'message' => $e->getMessage(), // <--- Ini pesan error aslinya
+            'ssl_config' => config('database.connections.mysql.options'),
+        ], 500);
+    }
+});
