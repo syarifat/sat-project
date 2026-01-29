32be6e801dbb3a432eeda2963844eafab1a47dfd
syarifat <syarifahsanit@gmail.com>
2026-01-29 20:45:14 +0700

    done 3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
